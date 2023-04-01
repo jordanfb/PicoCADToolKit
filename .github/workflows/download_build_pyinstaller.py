78b730d299d8aeb3b39628f833f21c707671676d
Jordan Faas-Bush <jmanfb@gmail.com>
2023-04-01 00:40:54 -0400

    Attempt to build pyinstaller binaries in github actions to avoid false positive virus result

---
+++ .github/workflows/download_build_pyinstaller.py
@@ -0,0 +1,27 @@
+# python 3.11
+
+
+# Download, unzip, and build the binaries for pyinstaller.
+# this is because many virus scanners get a false positive
+# for pyinstaller applications, but apparently building
+# my own binaries could help prevent that? Hopefully!
+
+import urllib.request as requests
+import zipfile
+import os
+from subprocess import call
+
+src_zip_url = "https://github.com/pyinstaller/pyinstaller/archive/refs/tags/v5.9.0.zip"
+subfolder_name = "pyinstaller-5.9.0" # the folder that will be unzipped from the download!
+
+filehandle, _ = requests.urlretrieve(src_zip_url)
+with zipfile.ZipFile(filehandle, 'r') as zf:
+	zf.extractall(".")
+
+# now run the ./bootloader/waf all python file
+print("Building bootloader:")
+call(["python", "waf", "all"], cwd=os.path.join(subfolder_name, "bootloader"))
+
+# now install it
+print("Installing PyInstaller")
+call(["python", "setup.py", "install"], cwd=subfolder_name)
